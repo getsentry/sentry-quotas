e3b681cfe030ed83aaf9fab4a273cecdb8fc4d4d
David Cramer <dcramer@gmail.com>
2012-10-29 12:07:59 -0700

    0.1.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
 
 setup(
     name='sentry-quotas',
-    version='0.1.3',
+    version='0.1.4',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/sentry-quotas',
